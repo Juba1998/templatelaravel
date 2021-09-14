4acf28adf05e4428011fcb25804d7e3b319d6b45
Juba1998 <juba.harf@gmail.com>
2021-09-14 12:19:52 +0200

    first commit

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,6 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<title>Checkout our layout</title>
+<!-- load bootstrap from a cdn -->
+<link href="/css/app.css" rel="stylesheet">
+<link href="/sass/app.css" rel="stylesheet">
